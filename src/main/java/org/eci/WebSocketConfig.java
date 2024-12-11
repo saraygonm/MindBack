78d3c151fe84efee19e20267dc68683c24884737
Saray Alieth Mendivelso Gonzalez <111905625+saraygonm@users.noreply.github.com>
2024-12-11 11:57:53 -0500

    Update WebSocketConfig.java

--- src/main/java/org/eci/WebSocketConfig.java
+++ src/main/java/org/eci/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/wss") // Cambia "/wss" por "/ws" (más estándar)
-                .setAllowedOrigins("https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net") // Reemplaza por tu dominio
+                .setAllowedOriginPatterns("https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net") // Reemplaza por tu dominio
                 .withSockJS(); // Habilita soporte para SockJS
     }
 }
